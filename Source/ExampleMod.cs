686cd901d9b4d70e55b59a8f69229f52a19a56a9
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-07-10 10:16:23 +0200

    add configuration example

--- Source/ExampleMod.cs
+++ Source/ExampleMod.cs
@@ -1,16 +1,22 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using NineSolsAPI;
 
 namespace ExampleMod;
 
 [BepInDependency(NineSolsAPICore.PluginGUID)]
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-public class ExampleMod : BaseUnityPlugin {
+public class ExampleMod : BaseUnityPlugin  {
+    // https://docs.bepinex.dev/articles/dev_guide/plugin_tutorial/4_configuration.html
+    private ConfigEntry<bool> enableSomethingConfig;
+
     private void Awake() {
         RCGLifeCycle.DontDestroyForever(gameObject);
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+        enableSomethingConfig = Config.Bind("General.Something", "Enable", true, "Enable the thing");
         
-        ToastManager.Toast("Example Toast Message");
+        ToastManager.Toast($"Something enable: {enableSomethingConfig.Value}");
     }
 
     private void OnDestroy() {
