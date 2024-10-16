2be5763afb9a6c5d6830377d6e121bdacacf9aef
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-10-16 16:28:29 +0200

    launch studio on start

--- Source/TasMod.cs
+++ Source/TasMod.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using BepInEx;
+using BepInEx.Configuration;
 using Cysharp.Threading.Tasks;
 using HarmonyLib;
 using JetBrains.Annotations;
 using NineSolsAPI;
+using StudioCommunication.Util;
 using TAS.Communication;
 using TAS.Module;
 using TAS.Utils;
@@ -18,11 +23,39 @@ public class TasMod : BaseUnityPlugin {
 
     private Harmony harmony = null!;
 
+    private ConfigEntry<bool> configOpenStudioOnLaunch = null!;
+    private ConfigEntry<KeyboardShortcut> configOpenStudioShortcut = null!;
+
+    private static void LaunchStudio() {
+        var path = Assembly.GetAssembly(typeof(TasMod)).Location;
+        if (path == "") return;
+
+        var studioPath = Path.Join(Path.GetDirectoryName(path) ?? "", "CelesteStudio.exe");
+        Log.Info($"Studio path at {studioPath}");
+
+        if (File.Exists(studioPath)) {
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo(studioPath) { UseShellExecute = true };
+            Log.Info("Trying to start");
+            var success = p.Start();
+            Log.Info($"Trying to start: {success}");
+        }
+    }
+
     private void Awake() {
         Log.Init(Logger);
         Instance = this;
 
         harmony = Harmony.CreateAndPatchAll(typeof(TasMod).Assembly);
+
+        configOpenStudioOnLaunch = Config.Bind("Studio", "Launch on start", true);
+        configOpenStudioShortcut = Config.Bind("Studio", "Launch", new KeyboardShortcut());
+
+        if (configOpenStudioOnLaunch.Value) {
+            LaunchStudio();
+        }
+
+        KeybindManager.Add(this, LaunchStudio, () => configOpenStudioShortcut.Value);
 
         TasSettings = new CelesteTasSettings(Config);
         RCGLifeCycle.DontDestroyForever(gameObject);
@@ -50,6 +83,7 @@ public class TasMod : BaseUnityPlugin {
     private class PlayerLoopItem(TasMod mb, Action action) : IPlayerLoopItem {
         public bool MoveNext() {
             if (!mb) return false;
+
             action();
             return true;
         }
